53eff3866d35fd864a6f967ca4b83a716be89aff
andrew <andrew@cloud-latitude.com>
2023-08-22 21:54:02 +0800

    format

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../app.service';
 
-// command: nest g controller cats to create a controller
+// command: "nest g controller cats" to create a controller
 @Controller('cats')
 export class CatsController {
   constructor(private readonly appService: AppService) {}
