4cf2ddc85ee5db0a10e135cd4d654d1343fca6ec
andrew <andrew@cloud-latitude.com>
2023-08-25 13:47:08 +0800

    path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatModule } from './cats/cat.module';
-import { HhhService } from './hhh/hhh.service';
+import { CatModule } from './cat/cat.module';
 
 @Module({
   imports: [CatModule],
   controllers: [AppController],
-  providers: [AppService, HhhService],
+  providers: [AppService],
 })
 export class AppModule {}
