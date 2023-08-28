381572dbbf93341ec35862277bfdaf8a188aeec3
andrew <andrew@cloud-latitude.com>
2023-08-28 10:32:06 +0800

    new Cat when update

--- src/cat/command/service/cat.service.ts
+++ src/cat/command/service/cat.service.ts
@@ -1,7 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Cat } from './cat.entity';
 import { CatRepository } from './cat.repository';
 import { CreateCatDto } from '../adapter/create-cat.dto';
+import { UpdateCatDto } from '../adapter/cat-update-dto';
+import { Cat } from './cat.entity';
 
 export const CAT_REPOSITORY = 'CAT_REPOSITORY';
 
@@ -13,12 +14,12 @@ export class CatService {
     @Inject(CAT_REPOSITORY) private readonly catRepository: CatRepository,
   ) {}
 
-  create(cat: CreateCatDto) {
+  create(cat: CreateCatDto): void {
     this.catRepository.create(CreateCatDto.toEntity(cat));
   }
 
-  update(index: number, cat: Cat): void {
-    this.catRepository.update(index, cat);
+  update(index: number, cat: UpdateCatDto): void {
+    this.catRepository.update(index, new Cat(cat.name, cat.age, cat.breed));
   }
 
   remove(index: number) {
